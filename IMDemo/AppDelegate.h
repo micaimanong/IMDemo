8bc8a7553e1e42158a9b782d7236784b858a294d
micaimanong <micaimanong@micaimanongdemacbook-pro.local>
2016-06-04 17:31:22 +0800

    Initial Commit

---
+++ IMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMDemo
+//
+//  Created by micaimanong on 16/6/4.
+//  Copyright © 2016年 迷彩码农. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMDemo
+//
+//  Created by micaimanong on 16/6/4.
+//  Copyright © 2016年 迷彩码农. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
